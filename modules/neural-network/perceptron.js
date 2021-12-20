45230049aac2a9ece7b49214e0b69d6e25a31485
Andrea Santona <andreasantonandreasantona.gi@gmail.com>
2021-12-20 16:12:04 +0100

    Initial state

---
+++ modules/neural-network/perceptron.js
@@ -0,0 +1,25 @@
+/** 
+ * The perceptron can communicates with other perceptrons 
+ * 
+ */
+class Perceptron {
+    constructor() {
+
+
+    }
+    /** Sum of the inputs */
+    sum() {
+
+    }
+    /** Sum of the inputs */
+    sum() {
+
+    }
+}
+class Weight {
+    constructor() {
+        /** The value multiplier of an input */
+        this.value = 1;
+    }
+
+}
